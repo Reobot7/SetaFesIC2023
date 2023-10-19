839bf490825a9b4d72c8cff1191cc42f86e06776
Reobot7 <Reobothand@gmail.com>
2023-10-20 01:50:45 +0900

    main.py update

--- main.py
+++ main.py
@@ -1,66 +1,122 @@
-import socket
-import time
-import json
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
+import signal
+import nfc
+import json
+from websocket_server import WebsocketServer
+import re
+import threading
+import time
 
-# NFCが利用可能かどうかをチェック
-try:
-    import nfc
-    NFC_ENABLED = True
-    print("テストモード: NFCリーダが接続されています。")
-except:
-    NFC_ENABLED = False
-    print("テストモード: NFCリーダが接続されていません。ダミーデータを使用します。")
+from enum import Enum, auto
 
-HOST, PORT = 'localhost', 65432
+class NFCState(Enum):
+    STOPPED = auto()
+    STARTED = auto()
+    
+nfc_state = NFCState.STOPPED  # NFCの初期状態をSTOPPEDとして設定
 
-def connect_socket():
-    """Socket接続を確立し、接続済みのsocketを返します"""
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    while True:
-        try:
-            s.connect((HOST, PORT))
-            print("Socket connection established.")
-            return s
-        except socket.error:
-            print("Failed to establish socket connection. Retrying in 2 seconds...")
-            time.sleep(2)
+def on_startup(targets):
+    return targets
 
-def send_data_via_socket(sock, data):
+def on_connect(tag):
+    print("Tag: {}".format(tag))
+    print("Tag type: {}".format(tag.type))
+
+def on_release(tag):
+    if tag.ndef:
+        print(tag.ndef.message.pretty())
+
+def send_dummy_data(server):
+    dummy_data = {"command": "detect", "message": "DUMMY_ID", "data_flag": "True"}
+    d = json.dumps(dummy_data, ensure_ascii=False, indent=2)
+    for client in server.clients:
+        server.send_message(client, d)
+        print(f"Sent dummy data to client {client['id']}: {d}")
+
+def readNFC(server):
+    global nfc_state
     try:
-        sock.sendall(json.dumps(data).encode())
-    except socket.error:
-        print("Socket connection lost. Reconnecting...")
-        sock = connect_socket()
-        send_data_via_socket(sock, data)
+        with nfc.ContactlessFrontend('usb') as clf:  
+            while True:
+                if nfc_state == NFCState.STARTED:
+                    tag = clf.connect(rdwr={
+                        'on-startup': on_startup,
+                        'on-connect': on_connect,
+                        'on-release': on_release,
+                        'targets': ['106A', '106B', '212F', '424F'],
+                        'iterations': 1,  # 試行回数を1回に設定
+                        'interval': 0.1,  # インターバルを0.1秒に設定
+                    })
 
-def read_from_nfc(sock):
-    with nfc.ContactlessFrontend('usb') as clf:
-        while True:
-            target = clf.sense(nfc.clf.RemoteTarget('106A'))
-            if target:
-                tag = nfc.tag.activate(clf, target)
-                tag_id = str(tag).split('=')[1]
-                send_data_via_socket(sock, {"data": tag_id})
-                time.sleep(1)
+                    if tag:
+                        ID_tmp = str(tag).find("ID=")
+                        ID_tmp1 = str(tag)[ID_tmp+len("ID="):]
+                        ID_arr = re.split("[= ]", str(ID_tmp1))
+                        message = str(ID_arr[0])
 
-def dummy_data_generator(sock):
-    dummy_ids = ["DUMMY_001", "DUMMY_002", "DUMMY_003"]
-    index = 0
-    while True:
-        send_data_via_socket(sock, {"data": dummy_ids[index]})
-        index = (index + 1) % len(dummy_ids)
-        time.sleep(5)
+                        print(f"Detected NFC ID: {message}")
+
+                        data = {"type": "nfc_data", "data": message}
+                        d = json.dumps(data, ensure_ascii=False, indent=2)
+
+                        for client in server.clients:
+                            server.send_message(client, d)
+                            print(f"Sent message to client {client['id']}: {d}")
+
+                        nfc_state = NFCState.STOPPED  # ここで状態をSTOPPEDに変更
+
+                    time.sleep(0.1)
+                else:
+                    time.sleep(1)  # 状態がSTOPPEDの場合は、1秒待機する
+    except nfc.UnsupportedTargetError:
+        print("Pasori is not connected. Sending dummy data...")
+        send_dummy_data(server)
+
+def handle_message(client, server, message):
+    global nfc_state
+    data = json.loads(message)
+    print(f"Received message: {message}")  # 追加: 受信したメッセージを表示
+    if "command" in data:
+        if data["command"] == "start_nfc":
+            nfc_state = NFCState.STARTED
+            print("NFC state set to STARTED")  # 追加: 状態変更を表示
+        elif data["command"] == "stop_nfc":
+            nfc_state = NFCState.STOPPED
+            print("NFC state set to STOPPED")  # 追加: 状態変更を表示
+
+def new_client(client, server):
+    print(f"New client connected: {client['id']}")
+    
+def close_resources(server, clf):
+    """クリーンアップ用の関数"""
+    print("Closing resources...")
+    if server:
+        # WebSocketサーバを停止
+        server.server_close()
+        print("WebSocket server closed.")
+    if clf:
+        # NFCデバイスとの接続を閉じる
+        clf.close()
+        print("NFC device connection closed.")
 
 def main():
-    sock = connect_socket()
-    try:
-        if NFC_ENABLED:
-            read_from_nfc(sock)
-        else:
-            dummy_data_generator(sock)
-    finally:
-        sock.close()
+    server = WebsocketServer(port=8765, host="127.0.0.1")
+    server.set_fn_new_client(new_client)
+    server.set_fn_message_received(handle_message)  # 追加
 
-if __name__ == "__main__":
+    # Start the NFC reading in a separate thread
+    threading.Thread(target=readNFC, args=(server,)).start()
+
+    server.run_forever()
+
+
+def handler(signal, frame):
+    print("Process Interrupt!")
+    sys.exit(0)
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, handler)
+    signal.signal(signal.SIGTERM, handler)
     main()
