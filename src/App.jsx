af036988a8cbacf904d98b97cd4982b6f6ed9198
Reobot7 <Reobothand@gmail.com>
2023-10-29 15:40:32 +0900

    stable

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, doc, getDoc,setDoc,serverTimestamp } from 'firebase/firestore';
+import { getFirestore, doc, getDoc,setDoc,deleteDoc,serverTimestamp } from 'firebase/firestore';
 
 const firebaseConfig = {
    apiKey: "AIzaSyC3CjfPL2JCGKIO3WCvEnnPmYT8YBm7faE",
@@ -26,6 +26,10 @@ function Settings({ setLocation }) {
     navigate("/waiting");
   };
 
+  const goToCheckResultWaiting = () => {
+    navigate("/checkresultwaiting");
+  };
+
   return (
     <div>
       <Header/>
@@ -34,11 +38,12 @@ function Settings({ setLocation }) {
       <button onClick={() => handleLocationSelect(3)}>3</button>
       <button onClick={() => handleLocationSelect(4)}>4</button>
       <button onClick={() => handleLocationSelect(5)}>5</button>
+      <button onClick={goToCheckResultWaiting}>結果確認待機画面に遷移</button>
     </div>
   );
 }
 
-function WaitingScreen({ setNFCData, websocket }) {
+function WaitingScreen({ setNFCData, websocket, location }) {
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -61,11 +66,29 @@ function WaitingScreen({ setNFCData, websocket }) {
     return () => {
       websocket.removeEventListener("message", handleDataReceived);
     };
-  }, [setNFCData, websocket, navigate]);  // この位置に依存配列を置く
+  }, [setNFCData, websocket, navigate]);
+
+  const getLocationName = () => {
+    switch(location) {
+      case 1:
+        return "1号館";
+      case 2:
+        return "72B教室";
+      case 3:
+        return "6号館";
+      case 4:
+        return "7号館";
+      case 5:
+        return "9号館";
+      default:
+        return "";
+    }
+  }
 
   return (
     <div>
       <Header />
+      <div>現在の場所: {getLocationName()}</div>
       <img src={`${window.location.origin}/C2622.png`} />
       <div className="message-waiting">ICカードをタッチしてください</div>
     </div>
@@ -157,6 +180,120 @@ function WaitingScreen({ setNFCData, websocket }) {
 
   }
 
+  // CheckResultWaitingScreen
+function CheckResultWaitingScreen({ setNFCData, websocket }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (websocket && websocket.readyState === WebSocket.OPEN) {
+      websocket.send(JSON.stringify({ command: "start_nfc" })); // 結果確認用のnfc開始信号
+    }
+
+    const handleDataReceived = (message) => {
+      const data = JSON.parse(message.data);
+      console.log('Received:', data);
+
+      if (data.type === "nfc_data") {
+        setNFCData(data);
+        navigate("/checkresultloading");
+      }
+    };
+
+    websocket.addEventListener("message", handleDataReceived);
+
+    return () => {
+      websocket.removeEventListener("message", handleDataReceived);
+    };
+  }, [setNFCData, websocket, navigate]);
+
+  return (
+    <div>
+      <Header />
+      <img src={`${window.location.origin}/C2622.png`} />
+      <div className="message-waiting">結果を確認するためにICカードをタッチしてください</div>
+    </div>
+  );
+}
+
+// CheckResultLoadingScreen
+function CheckResultLoadingScreen({ nfcData, setNFCResult }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const docRef = doc(db, 'nfcIDs', nfcData.data);
+      const docSnap = await getDoc(docRef);
+      
+
+      const docSnap_result = await getDoc(docRef);
+      setNFCResult(docSnap_result.data());
+      const timer = setTimeout(() => {
+        navigate("/checkresult");
+      }, 2000);
+    };
+
+    if (nfcData && nfcData.data) {
+      fetchData();
+    }
+  }, [nfcData, setNFCResult, navigate]);
+
+  return (
+    <div>
+      <Header />
+      <img src={`${window.location.origin}/D8739.png`}/>
+      <div className="message-loading">結果を読み取り中...</div>
+    </div>
+  );
+}
+
+function CheckResultScreen({ nfcResult,nfcData }) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/checkresultwaiting");
+    }, 5000); // 5秒後に結果確認専用の待機画面に遷移
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  // OKの数を数える
+  const okCount = [1, 2, 3, 4, 5].filter(num => nfcResult && nfcResult[num] && nfcResult[num].state).length;
+  console.log(nfcResult)
+  // Firestoreからの結果を削除
+  const deleteResultFromFirestore = async () => {
+    const docRef = doc(db, 'nfcIDs', nfcData.data); 
+    await deleteDoc(docRef);
+  };
+  deleteResultFromFirestore(); // 削除処理を呼び出し
+
+  return (
+    <div>
+      <Header /> 
+      <h2>結果発表</h2>
+      <p>{`5個中${okCount}個のOKがあります。`}</p>
+      <table>
+      <tbody>
+          <tr>
+            <td className="table-cell">1号館</td>
+            <td className="table-cell">72B教室</td>
+            <td className="table-cell">6号館</td>
+            <td className="table-cell">7号館</td>
+            <td className="table-cell">9号館</td>
+          </tr>
+          <tr>
+            <td className="table-cell"> {nfcResult && nfcResult[1] && nfcResult[1].state ? "OK" : ""}</td>
+            <td className="table-cell"> {nfcResult && nfcResult[2] && nfcResult[2].state ? "OK" : ""}</td>
+            <td className="table-cell"> {nfcResult && nfcResult[3] && nfcResult[3].state ? "OK" : ""}</td>
+            <td className="table-cell"> {nfcResult && nfcResult[4] && nfcResult[4].state ? "OK" : ""}</td>
+            <td className="table-cell"> {nfcResult && nfcResult[5] && nfcResult[5].state ? "OK" : ""}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
   function Header() {
     return (
       <header>
@@ -170,6 +307,8 @@ function App() {
   const [location, setLocation] = useState(null);
   const [nfcData, setNFCData] = useState(null);
   const [nfcResult, setNFCResult] = useState(null);
+  const [nfcCheckResult, setNFCCheckResult] = useState(null);
+ 
   const [websocket, setWebsocket] = useState(null);
 
   useEffect(() => {
@@ -199,9 +338,12 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Settings setLocation={setLocation} />} />
-        <Route path="/waiting" element={<WaitingScreen setNFCData={setNFCData} websocket={websocket} />} />
+        <Route path="/waiting" element={<WaitingScreen setNFCData={setNFCData} websocket={websocket} location={location}/>} />
         <Route path="/loading" element={<LoadingScreen nfcData={nfcData} setNFCResult={setNFCResult} location={location} />} />
         <Route path="/result" element={<ResultScreen nfcResult={nfcResult} />} />
+        <Route path="/checkresultwaiting" element={<CheckResultWaitingScreen setNFCData={setNFCData} websocket={websocket} />} />
+          <Route path="/checkresultloading" element={<CheckResultLoadingScreen nfcData={nfcData} setNFCResult={setNFCResult} />} />
+            <Route path="/checkresult" element={<CheckResultScreen nfcResult={nfcResult} nfcData={nfcData} />} />
       </Routes>
     </Router>
   );
